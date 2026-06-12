036fbe3063622a09655723b96ea9284e5e6005fd
arsmoriendy <nugrahasteven@gmail.com>
2026-06-12 10:58:14 +0700

    feat: docker config util

--- src/prelude.rs
+++ src/prelude.rs
@@ -1,4 +1,7 @@
 pub use anyhow::{Context as ErrorCtx, Result};
 pub use serde::{Deserialize, Serialize};
+pub use std::collections::HashMap;
+pub use std::fs;
+pub use std::path::Path;
 pub use std::process::Command;
 pub use thiserror::Error;
--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -1,8 +1,3 @@
-macro_rules! docker {
-    ($($args:expr),+) => {
-        Command::new("docker")
-            .args([$($args),+])
-            .output()
-            .with_context(|| "Failed spawning docker, make sure docker is installed")?
-    };
-}
+#[macro_use]
+mod macros;
+pub mod docker_config;
