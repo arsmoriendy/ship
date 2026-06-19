08fd5ca4e99af60065c8af2e421b34f2251e0ab0
arsmoriendy <nugrahasteven@gmail.com>
2026-06-19 16:58:39 +0700

    feat: create config if not exist

--- src/config/mod.rs
+++ src/config/mod.rs
@@ -1,13 +1,13 @@
-use crate::{image::Image, prelude::*, project::Project, utils::project_dirs};
+use crate::{image::Image, prelude::*, project::Project};
 
-#[derive(Deserialize)]
+#[derive(Deserialize, Default, Serialize)]
 #[serde(rename_all = "camelCase")]
 pub struct Config {
     pub project_registries: HashMap<String, String>,
     pub registry_commands: HashMap<String, RegistryCommands>,
 }
 
-#[derive(Deserialize)]
+#[derive(Deserialize, Serialize)]
 #[serde(rename_all = "camelCase")]
 pub struct RegistryCommands {
     delete_image: String,
@@ -54,8 +54,20 @@ impl RegistryCommands {
 
 impl Config {
     pub fn new() -> Result<Self> {
+        let config_file = config_path()?;
+
+        if !config_file.exists() {
+            let config_parent = config_file
+                .parent()
+                .with_context(|| anyhow!("failed to retrieve config file dirname"))?;
+            fs::create_dir_all(config_parent)?;
+            let config = Self::default();
+            fs::write(config_file, serde_json::to_string(&config)?)?;
+            return Ok(config);
+        }
+
         let config_str =
-            fs::read_to_string(config_path()?).with_context(|| "failed to read config file")?;
+            fs::read_to_string(config_file).with_context(|| "failed to read config file")?;
         Ok(serde_json::from_str(&config_str).with_context(|| "failed to parse config file")?)
     }
 }
