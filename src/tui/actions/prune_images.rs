eaec21e5c9b44ea8fed09115ba67d03dce7f38d3
arsmoriendy <nugrahasteven@gmail.com>
2026-08-07 10:28:00 +0700

    fix: only prune pushed images

--- src/tui/actions/prune_images.rs
+++ src/tui/actions/prune_images.rs
@@ -1,6 +1,12 @@
+use crate::image::Image;
+
 add_app_action!(slf; prune_images, terminal, {
     let mtx = slf.state.lock().await;
-    let images = mtx.selected_project().images.clone();
+    let Some(digests)=mtx.store.project_registry_digests.get(mtx.selected_project().name.as_str())else { return Ok(()) };
+    let images: Vec<Image> = mtx.selected_project().images.clone().into_iter().filter(|i| {
+        let Some(digest)=i.digest else {return false};
+        digests.contains(&digest)
+    }).collect();
     let behaviour = mtx.config.command_behaviours.delete_image.clone();
     drop(mtx);
 
