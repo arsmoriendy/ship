20b81131093845040c0efb9c082d5385973b2153
arsmoriendy <nugrahasteven@gmail.com>
2026-08-08 10:23:13 +0700

    feat: cli helper

--- src/main.rs
+++ src/main.rs
@@ -6,10 +6,16 @@ mod project;
 mod store;
 mod tui;
 
+use clap::Parser;
 use prelude::*;
+
+#[derive(Parser)]
+#[command(version, about)]
+struct Args {}
 
 #[tokio::main]
 async fn main() -> Result<()> {
+    let _ = Args::parse();
     let mut app = tui::App::new()?;
     let mut terminal = ratatui::init();
     app.run(&mut terminal).await?;
